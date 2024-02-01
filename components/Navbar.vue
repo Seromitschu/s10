a09ef28baf28940b3169824daf69279e13c3a46b
Serhat <137821422+Seromitschu@users.noreply.github.com>
2024-02-01 22:38:27 +0300

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,6 +3,5 @@
     <p class="border-red-500 border-b-2 border-opacity-100 hover:border-opacity-70 navbarBtn thinText display-inline-block mr-5"><a href="/">Home</a></p>
     <p class="border-green-500 border-b-2 border-opacity-100 hover:border-opacity-70 navbarBtn thinText display-inline-block mr-5"><a href="/about">About</a></p>
     <p class="border-blue-500 border-b-2 border-opacity-100 hover:border-opacity-70 navbarBtn thinText display-inline-block mr-5"><a href="/contact">Contact</a></p>
-    <p class="border-yellow-500 border-b-2 border-opacity-100 hover:border-opacity-70 navbarBtn thinText display-inline-block"><a href="/activities">Activities</a></p>
     </div>
 </template>
