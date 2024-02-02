79a736cfe3a21d4b21599a36be0c4a6995a496ee
Serhat <137821422+Seromitschu@users.noreply.github.com>
2024-02-02 19:59:04 +0300

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="navbar padding mt-9">
+<div class="navbar padding mt-15">
     <p class="boldText" style="font-weight: 500;">Hi, I’m Serhat 👋<br><p class="normalText opacity-40">⭐ Self-taught web and app developer.</p></p>
   </div>
 </template>
