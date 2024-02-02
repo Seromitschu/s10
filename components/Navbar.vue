5db575191f2053dbac2cf5e057eda0de4e4a4e41
Serhat <137821422+Seromitschu@users.noreply.github.com>
2024-02-02 20:13:25 +0300

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="navbar padding mt-9 opacity-80">
-    <p class="homeBtn navbarBtn thinText display-inline-block mr-3"><a class="generalBtnText opacity-80" href="/">Home</a></p>
-    <p class="aboutBtn navbarBtn thinText display-inline-block mr-3"><a class="generalBtnText opacity-80" href="/about">About</a></p>
-    <p class="contactBtn navbarBtn thinText display-inline-block"><a class="generalBtnText opacity-80" href="/contact">Contact</a></p>
+    <p class="homeBtn navbarBtn thinText display-inline-block mr-3"><a class="generalBtnText opacity-100" href="/">Home</a></p>
+    <p class="aboutBtn navbarBtn thinText display-inline-block mr-3"><a class="generalBtnText opacity-100" href="/about">About</a></p>
+    <p class="contactBtn navbarBtn thinText display-inline-block"><a class="generalBtnText opacity-100" href="/contact">Contact</a></p>
     </div>
 </template>
