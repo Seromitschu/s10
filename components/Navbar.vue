61ca54ea16089279350cac49437e1d6a31838afd
Serhat <137821422+Seromitschu@users.noreply.github.com>
2024-02-02 20:24:12 +0300

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="navbar padding mt-9 opacity-80">
-    <p class="homeBtn navbarBtn thinText display-inline-block mr-3 hover:opacity-40"><a class="generalBtnText opacity-100" href="/">Home</a></p>
-    <p class="aboutBtn navbarBtn thinText display-inline-block mr-3 hover:opacity-40"><a class="generalBtnText opacity-100" href="/about">About</a></p>
-    <p class="contactBtn navbarBtn thinText display-inline-block hover:opacity-40"><a class="generalBtnText opacity-100" href="/contact">Contact</a></p>
+    <div class="navbar padding mt-9 opacity-100">
+    <p class="homeBtn navbarBtn thinText display-inline-block mr-3 hover:opacity-70"><a class="generalBtnText opacity-100" href="/">Home</a></p>
+    <p class="aboutBtn navbarBtn thinText display-inline-block mr-3 hover:opacity-70"><a class="generalBtnText opacity-100" href="/about">About</a></p>
+    <p class="contactBtn navbarBtn thinText display-inline-block hover:opacity-70"><a class="generalBtnText opacity-100" href="/contact">Contact</a></p>
     </div>
 </template>
